fc319206f3fde4560a8a7303c416e311e031f1b9
Yaroslav Hladun <yaroslav@getsomatic.com>
2024-04-29 14:39:53 +0300

    implemented FK

---
+++ fk.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+from math import pi
+from typing import Tuple
+
+JOINTS_LOWER_LIMIT = [-2.0 * pi, -2.059, -2.0 * pi, -0.19198, -2.0 * pi, -1.69297, -2.0 * pi]
+JOINTS_UPPER_LIMIT = [2.0 * pi, 2.0944, 2.0 * pi, 3.927, 2.0 * pi, pi, 2.0 * pi]
+
+
+class JointValuesScaler(nn.Module):
+    def __init__(self, device):
+        super(JointValuesScaler, self).__init__()
+        self.joints_min = torch.tensor(JOINTS_LOWER_LIMIT).to(device)
+        self.joints_max = torch.tensor(JOINTS_UPPER_LIMIT).to(device)
+
+    def forward(self, x):
+        return (x - self.joints_min) / (self.joints_max - self.joints_min)
+
+
+class JointValuesScalerInverse(nn.Module):
+
+    def __init__(self, device):
+        super(JointValuesScalerInverse, self).__init__()
+        self.joints_min = torch.tensor(JOINTS_LOWER_LIMIT).to(device)
+        self.joints_max = torch.tensor(JOINTS_UPPER_LIMIT).to(device)
+
+    def forward(self, x):
+        return x * (self.joints_max - self.joints_min) + self.joints_min
+
+
+class TransformationUtility:
+    @staticmethod
+    def xyz_rpy_to_torch_affine(xyzrpy: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Converts a tensor of xyzrpy values to a tensor of 4x4 affine matrices
+        :param xyzrpy: shape (N, 6) tensor of xyzrpy values
+        :return: two torch tensors;
+        The first tensor is the rotation matrix and the second tensor is the translation vector
+        """
+        x, y, z, roll, pitch, yaw = xyzrpy[:, 0], xyzrpy[:, 1], xyzrpy[:, 2], xyzrpy[:, 3], xyzrpy[:, 4], xyzrpy[:, 5]
+
+        Rz = TransformationUtility.rotation_z(yaw)
+        Ry = TransformationUtility.rotation_y(pitch)
+        Rx = TransformationUtility.rotation_x(roll)
+
+        R = torch.matmul(torch.matmul(Rz, Ry), Rx)
+        t = torch.stack([x, y, z], dim=1).unsqueeze(-1)
+
+        return R, t
+
+    @staticmethod
+    def rotation_x(angle: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the rotation matrix around the x-axis
+        :param angle: the angle in radians; shape (...,)
+        :return: the rotation matrix; shape (..., 3, 3)
+        """
+        cx = torch.cos(angle)
+        sx = torch.sin(angle)
+
+        return torch.stack([
+            torch.stack([torch.ones_like(cx), torch.zeros_like(cx), torch.zeros_like(cx)], dim=-1),
+            torch.stack([torch.zeros_like(cx), cx, -sx], dim=-1),
+            torch.stack([torch.zeros_like(cx), sx, cx], dim=-1)
+        ], dim=-2)
+
+    @staticmethod
+    def rotation_y(angle: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the rotation matrix around the y-axis
+        :param angle: the angle in radians; shape (...,)
+        :return: the rotation matrix; shape (..., 3, 3)
+        """
+        cy = torch.cos(angle)
+        sy = torch.sin(angle)
+
+        return torch.stack([
+            torch.stack([cy, torch.zeros_like(cy), sy], dim=-1),
+            torch.stack([torch.zeros_like(cy), torch.ones_like(cy), torch.zeros_like(cy)], dim=-1),
+            torch.stack([-sy, torch.zeros_like(cy), cy], dim=-1)
+        ], dim=-2)
+
+    @staticmethod
+    def rotation_z(angle: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the rotation matrix around the z-axis
+        :param angle: the angle in radians; shape (...,)
+        :return: the rotation matrix; shape (..., 3, 3)
+        """
+        cz = torch.cos(angle)
+        sz = torch.sin(angle)
+
+        zero = torch.zeros_like(cz)
+        one = torch.ones_like(cz)
+
+        return torch.stack([
+            torch.stack([cz, -sz, zero], dim=-1),
+            torch.stack([sz, cz, zero], dim=-1),
+            torch.stack([zero, zero, one], dim=-1)
+        ], dim=-2)
+
+    @staticmethod
+    def rotation_to_affine(rotation: torch.Tensor) -> torch.Tensor:
+        """
+        Converts a tensor of rotation matrices to a tensor of 4x4 affine matrices
+        :param rotation: shape (N, 3, 3) tensor of rotation matrices
+        :return: shape (N, 4, 4) tensor of affine matrices
+        """
+        affine_matrices = torch.zeros(rotation.shape[0], 4, 4, device=rotation.device, dtype=rotation.dtype)
+        affine_matrices[:, :3, :3] = rotation
+        affine_matrices[:, 3, 3] = 1
+        return affine_matrices
+
+
+class FK(nn.Module):
+    def __init__(self, device):
+        super(FK, self).__init__()
+
+        transformations = torch.tensor([
+            [0, 0, 0.267, 0, 0, 0],
+            [0, 0, 0, -1.5708, 0, 0],
+            [0, -0.293, 0, 1.5708, 0, 0],
+            [0.0525, 0, 0, 1.5708, 0, 0],
+            [0.0775, -0.3425, 0, 1.5708, 0, 0],
+            [0, 0, 0, 1.5708, 0, 0],
+            [0.076, 0.097, 0, -1.5708, 0, 0]
+        ]).to(device)
+
+        self.joint_transforms = TransformationUtility.xyz_rpy_to_torch_affine(transformations)
+
+    def forward(self, x):
+        R_result = torch.eye(3, device=x.device).unsqueeze(0).expand(x.size(0), 3, 3)
+        t_result = torch.zeros(x.size(0), 3, 1, device=x.device)
+
+        joint_rotations = TransformationUtility.rotation_z(x)  # 3x3 rotation matrices
+
+        for i in range(7):
+            R, t = self.joint_transforms[0][i], self.joint_transforms[1][i]
+            t_result = torch.matmul(R_result, t) + t_result
+            R_result = torch.matmul(R_result, R)
+            R_j = joint_rotations[:, i]
+            R_result = torch.matmul(R_result, R_j)
+
+            # result = torch.matmul(result, joint_rotations[i].unsqueeze(0).expand(x.size(0), 4, 4))
+
+        return R_result, t_result
---
+++ fk_test.py
@@ -0,0 +1,71 @@
+import unittest
+
+import torch
+
+from fk import *
+
+
+class TransformationUtilityTest(unittest.TestCase):
+
+    def test_xyz_rpy_to_torch_affine(self):
+        pass
+
+
+class FKTest(unittest.TestCase):
+    def test_zero_state(self):
+        fk = FK('cpu')
+        zero_state = torch.zeros(1, 7)
+        R_result, t_result = fk(zero_state)
+
+        self.assertEqual(torch.Size([1, 3, 3]), R_result.shape)
+        self.assertEqual(torch.Size([1, 3, 1]), t_result.shape)
+
+        target_position = torch.Tensor([[[2.0600e-01], [3.3539e-06], [1.2050e-01]]])
+
+        self.assertTrue(torch.allclose(target_position, t_result, atol=1e-3))
+
+    def test_initial_state(self):
+        fk = FK('cpu')
+        initial_state = torch.Tensor([[0, 0.262, -3.142, 2.618, 0, 1.309, 0]])
+        R_result, t_result = fk(initial_state)
+
+        self.assertEqual(torch.Size([1, 3, 3]), R_result.shape)
+        self.assertEqual(torch.Size([1, 3, 1]), t_result.shape)
+
+        target_position = torch.Tensor([[[-0.086], [0], [0.990]]])
+
+        self.assertTrue(torch.allclose(target_position, t_result, atol=1e-3))
+
+    def test_forward_state(self):
+        fk = FK('cpu')
+        forward_state = torch.Tensor([[-3.142, 0.262, -3.046, 2.316, -0.192, 2.499, 0.710]])
+        R_result, t_result = fk(forward_state)
+
+        self.assertEqual(torch.Size([1, 3, 3]), R_result.shape)
+        self.assertEqual(torch.Size([1, 3, 1]), t_result.shape)
+
+        target_position = torch.Tensor([[[0.1746], [0.0067], [0.8052]]])
+
+        self.assertTrue(torch.allclose(target_position, t_result, atol=1e-3))
+
+    def test_multiple_states(self):
+        fk = FK('cpu')
+        states = torch.Tensor([
+            [0, 0.262, -3.142, 2.618, 0, 1.309, 0],
+            [-3.142, 0.262, -3.046, 2.316, -0.192, 2.499, 0.710]
+        ])
+        R_result, t_result = fk(states)
+
+        self.assertEqual(torch.Size([2, 3, 3]), R_result.shape)
+        self.assertEqual(torch.Size([2, 3, 1]), t_result.shape)
+
+        target_positions = torch.Tensor([
+            [[-0.086], [0], [0.990]],
+            [[0.1746], [0.0067], [0.8052]]
+        ])
+
+        self.assertTrue(torch.allclose(target_positions, t_result, atol=1e-3))
+
+
+if __name__ == '__main__':
+    unittest.main()
