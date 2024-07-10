596153cd2fa0996c624073336db0a23ad3576be7
Yaroslav Hladun <yaroslav@getsomatic.com>
2024-07-10 11:28:44 +0300

    save

--- seed_epsilon_ik_config.py
+++ seed_epsilon_ik_config.py
@@ -6,7 +6,7 @@ class SeedEpsilonIKConfig:
     max_seed_dist = 90 * math.pi / 180
 
     # model
-    d_model = 1024
+    d_model = 2048
 
     # sampling dataset for nearest ik
     upper_bound = 0.005
